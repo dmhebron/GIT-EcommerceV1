3d0a04bb442203a95f3f843bca2e4453c0d90a8f
dmhebron <dmhebron21@gmail.com>
2022-10-10 21:09:22 -0400

    started json file

---
+++ products/products.js
@@ -0,0 +1,13 @@
+let text = '{"products":['
+    '{"product1": "Emergency Mechanical Services"},' +
+    '{"product2": "Emergency Tire Replacement Services"},' +
+    '{"product3": "Towing Services"},' +
+    '{"product4": "Tractor Tire Discount Program"},' +
+    '{"product5": "Trailer Tire Discount Program"},' +
+    '{"product6": "Trailer Rental"},' +
+    '{"product7": "GPS"},' +
+    '{"product8": "Dash Camera"},' +
+    '{"product9": "Headset"} ]}';
+   
+    const obj = JSON.parse(text);
+    
