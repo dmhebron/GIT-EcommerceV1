9ae0075ae48fce7c099afd737c42d4ce8657b269
dmhebron <dmhebron21@gmail.com>
2022-10-10 23:19:45 -0400

    added 12 products

--- products/products.js
+++ products/products.js
@@ -12,8 +12,12 @@ var content = {
         "Headset",
     ],
 
-    "price": [500, 450, 100, 400, 300, 250, 60, 75, 50],
-    "merchandise": {}
+    "price": [500, 450, 100, 400, 300, 250, 60, 75, 50, 20, 15, 30],
+    "merchandise": [
+        "T-shirts",
+        "Tumbler",
+        "Hoodie",
+    ]
 }
 var output = document.getElementById('output');
 output.innerHTML = content.product[0] + ' ' + '$' + content.price[0] + '/hour'
@@ -42,3 +46,11 @@ output.innerHTML = content.product[7] + ' ' + '$' + content.price[7]
 var output = document.getElementById('output9');
 output.innerHTML = content.product[8] + ' ' + '$' + content.price[8]
 
+var output = document.getElementById('output10');
+output.innerHTML = content.merchandise[0] + ' ' + '$' + content.price[9]
+
+var output = document.getElementById('output11');
+output.innerHTML = content.merchandise[1] + ' ' + '$' + content.price[10]
+
+var output = document.getElementById('output12');
+output.innerHTML = content.merchandise[2] + ' ' + '$' + content.price[11]
