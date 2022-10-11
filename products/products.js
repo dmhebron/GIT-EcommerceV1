32bc5d0f0757249296523be057734e7b477477bc
dmhebron <dmhebron21@gmail.com>
2022-10-10 22:52:19 -0400

    added products

--- products/products.js
+++ products/products.js
@@ -1,18 +1,44 @@
    
 var content = {
-    "product1": "Emergency Mechanical Services",
-    "product2": "Emergency Tire Replacement Services",
-    "product3": "Towing Services",
-    "product4": "Tractor Tire Discount Program",
-    "product5": "Trailer Tire Discount Program",
-    "product6": "Trailer Rental",
-    "product7": "GPS", 
-    "product8": "Dash Camera", 
-    "product9": "Headset",
+    "product": [
+        "Emergency Mechanical Services", 
+        "Emergency Tire Replacement Services", 
+        "Towing Services", 
+        "Tractor Tire Discount Program",
+        "Trailer Tire Discount Program",
+        "Trailer Rental",
+        "GPS",
+        "Dash Camera", 
+        "Headset",
+    ],
+
     "price": [500, 450, 100, 400, 300, 250, 60, 75, 50],
     "merchandise": {}
 }
 var output = document.getElementById('output');
-output.innerHTML = content.product1 + ' ' + '$' + content.price[0] + '/hour'
+output.innerHTML = content.product[0] + ' ' + '$' + content.price[0] + '/hour'
+
 var output = document.getElementById('output2');
-output.innerHTML = content.product2
+output.innerHTML = content.product[1] + ' ' + '$' + content.price[1] + '/hour'
+
+var output = document.getElementById('output3');
+output.innerHTML = content.product[2] + ' ' + '$' + content.price[2] + '/mile'
+
+var output = document.getElementById('output4');
+output.innerHTML = content.product[3] + ' ' + '$' + content.price[3] + '/tire'
+
+var output = document.getElementById('output5');
+output.innerHTML = content.product[4] + ' ' + '$' + content.price[4] + '/tire'
+
+var output = document.getElementById('output6');
+output.innerHTML = content.product[5] + ' ' + '$' + content.price[5] + '/load'
+
+var output = document.getElementById('output7');
+output.innerHTML = content.product[6] + ' ' + '$' + content.price[6]
+
+var output = document.getElementById('output8');
+output.innerHTML = content.product[7] + ' ' + '$' + content.price[7]
+
+var output = document.getElementById('output9');
+output.innerHTML = content.product[8] + ' ' + '$' + content.price[8]
+
