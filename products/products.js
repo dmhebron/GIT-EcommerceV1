eafccb08d15fcdcf936d4f9bf235bd6f6417ffc1
dmhebron <dmhebron21@gmail.com>
2022-10-10 22:32:04 -0400

    added first product

--- products/products.js
+++ products/products.js
@@ -1,13 +1,18 @@
-let text = '{"products":['
-    '{"product1": "Emergency Mechanical Services"},' +
-    '{"product2": "Emergency Tire Replacement Services"},' +
-    '{"product3": "Towing Services"},' +
-    '{"product4": "Tractor Tire Discount Program"},' +
-    '{"product5": "Trailer Tire Discount Program"},' +
-    '{"product6": "Trailer Rental"},' +
-    '{"product7": "GPS"},' +
-    '{"product8": "Dash Camera"},' +
-    '{"product9": "Headset"} ]}';
    
-    const obj = JSON.parse(text);
-    
+var content = {
+    "product1": "Emergency Mechanical Services",
+    "product2": "Emergency Tire Replacement Services",
+    "product3": "Towing Services",
+    "product4": "Tractor Tire Discount Program",
+    "product5": "Trailer Tire Discount Program",
+    "product6": "Trailer Rental",
+    "product7": "GPS", 
+    "product8": "Dash Camera", 
+    "product9": "Headset",
+    "price": [500, 450, 100, 400, 300, 250, 60, 75, 50],
+    "merchandise": {}
+}
+var output = document.getElementById('output');
+output.innerHTML = content.product1 + ' ' + '$' + content.price[0] + '/hour'
+var output = document.getElementById('output2');
+output.innerHTML = content.product2
